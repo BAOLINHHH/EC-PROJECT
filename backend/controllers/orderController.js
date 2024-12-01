d653c76a9aa67f19eb946868f8da1a1dde5d62b8
sontanhoang2001 <sontanhoang2001@gmail.com>
2024-12-01 12:18:50 +0700

    api create_payment_url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
-import querystring  from "qs";
+import querystring from "qs";
 import crypto from "crypto";
 import dateFormat from "dateformat";
+import moment from "moment";
 
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
@@ -251,60 +252,129 @@ const getOrderByTrackingCode = asyncHandler(async (req, res) => {
   res.json(order); // Trả về thông tin đơn hàng
 });
 
+
 // @desc     create payment url
 // @route    POST /api/orders/create_payment_url
 // @access   Privie
 const createPaymentUrl = asyncHandler(async (req, res) => {
-  var ipAddr =
+  // var ipAddr =
+  //   req.headers["x-forwarded-for"] ||
+  //   req.connection.remoteAddress ||
+  //   req.socket.remoteAddress ||
+  //   req.connection.socket.remoteAddress;
+
+  // var date = new Date();
+  // var createDate = dateFormat(date, "yyyymmddHHmmss");
+  // var orderId = dateFormat(date, "HHmmss");
+  // var amount = req.body.amount;
+  // var bankCode = req.body.bankCode;
+
+  // var orderInfo = req.body.orderDescription;
+  // var orderType = req.body.orderType;
+  // var locale = req.body.language || "vn";
+  // var currCode = "VND";
+
+  // var vnp_Params = {
+  //   vnp_Version: "2.1.0",
+  //   vnp_Command: "pay",
+  //   vnp_TmnCode: process.env.VNP_TMNCODE,
+  //   vnp_Locale: locale,
+  //   vnp_CurrCode: currCode,
+  //   vnp_TxnRef: orderId,
+  //   vnp_OrderInfo: orderInfo,
+  //   vnp_OrderType: orderType,
+  //   vnp_Amount: amount * 100,
+  //   vnp_ReturnUrl: process.env.VNP_RETURNURL,
+  //   vnp_IpAddr: "127.0.0.1",
+  //   vnp_CreateDate: createDate,
+  // };
+
+  // if (bankCode) {
+  //   vnp_Params["vnp_BankCode"] = bankCode;
+  // }
+
+  // var signData = querystring.stringify(vnp_Params, { encode: false });
+  // var hmac = crypto.createHmac("sha512", process.env.VNP_HASHSECRET);
+  // var signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
+  // vnp_Params["vnp_SecureHash"] = signed;
+  // const paymentUrl =
+  //   process.env.VNP_URL +
+  //   "?" +
+  //   querystring.stringify(vnp_Params, { encode: false });
+
+  //   console.log("paymentUrl", paymentUrl)
+  //   console.log("vnp_Params", vnp_Params)
+
+  // res.redirect(paymentUrl);
+
+  process.env.TZ = "Asia/Ho_Chi_Minh";
+
+  let date = new Date();
+  let createDate = moment(date).format("YYYYMMDDHHmmss");
+
+  let ipAddr =
     req.headers["x-forwarded-for"] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
 
-  var date = new Date();
-  var createDate = dateFormat(date, "yyyymmddHHmmss");
-  var orderId = dateFormat(date, "HHmmss");
-  var amount = req.body.amount;
-  var bankCode = req.body.bankCode;
+  let tmnCode = process.env.VNP_TMNCODE;
+  let secretKey = process.env.VNP_HASHSECRET;
+  let vnpUrl = process.env.VNP_URL;
+  let returnUrl = process.env.VNP_RETURNURL;
+  let orderId = moment(date).format("DDHHmmss");
+  let amount = req.body.amount;
+  let bankCode = req.body.bankCode;
 
-  var orderInfo = req.body.orderDescription;
-  var orderType = req.body.orderType;
-  var locale = req.body.language || "vn";
-  var currCode = "VND";
-
-  var vnp_Params = {
-    vnp_Version: "2.1.0",
-    vnp_Command: "pay",
-    vnp_TmnCode: process.env.VNP_TMNCODE,
-    vnp_Locale: locale,
-    vnp_CurrCode: currCode,
-    vnp_TxnRef: orderId,
-    vnp_OrderInfo: orderInfo,
-    vnp_OrderType: orderType,
-    vnp_Amount: amount * 100,
-    vnp_ReturnUrl: process.env.VNP_RETURNURL,
-    vnp_IpAddr: "127.0.0.1",
-    vnp_CreateDate: createDate,
-  };
-
-  if (bankCode) {
+  let locale = req.body.language;
+  if (locale === null || locale === "") {
+    locale = "vn";
+  }
+  let currCode = "VND";
+  let vnp_Params = {};
+  vnp_Params["vnp_Version"] = "2.1.0";
+  vnp_Params["vnp_Command"] = "pay";
+  vnp_Params["vnp_TmnCode"] = tmnCode;
+  vnp_Params["vnp_Locale"] = locale;
+  vnp_Params["vnp_CurrCode"] = currCode;
+  vnp_Params["vnp_TxnRef"] = orderId;
+  vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+  vnp_Params["vnp_OrderType"] = "other";
+  vnp_Params["vnp_Amount"] = amount * 100;
+  vnp_Params["vnp_ReturnUrl"] = returnUrl;
+  vnp_Params["vnp_IpAddr"] = ipAddr;
+  vnp_Params["vnp_CreateDate"] = createDate;
+  if (bankCode !== null && bankCode !== "") {
     vnp_Params["vnp_BankCode"] = bankCode;
   }
 
-  var signData = querystring.stringify(vnp_Params, { encode: false });
-  var hmac = crypto.createHmac("sha512", process.env.VNP_HASHSECRET);
-  var signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
+  vnp_Params = sortObject(vnp_Params);
+
+  let signData = querystring.stringify(vnp_Params, { encode: false });
+  let hmac = crypto.createHmac("sha512", secretKey);
+  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
   vnp_Params["vnp_SecureHash"] = signed;
-  const paymentUrl =
-    process.env.VNP_URL +
-    "?" +
-    querystring.stringify(vnp_Params, { encode: false });
-
-    console.log("paymentUrl", paymentUrl)
-    console.log("vnp_Params", vnp_Params)
-
-  res.redirect(paymentUrl);
+  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+  // console.log(vnpUrl); // Log URL cuối cùng
+  // res.redirect(vnpUrl);
+  res.status(200).json({ vnpUrl });
 });
+
+const sortObject = (obj) => {
+  let sorted = {};
+  let str = [];
+  let key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key));
+    }
+  }
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
+  return sorted;
+};
 
 export {
   addOrderItems,
